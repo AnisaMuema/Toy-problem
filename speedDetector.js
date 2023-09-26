06669555095decab6299a94e0babf3ee104af0fa
anisamuema <anisa.muema.school@gmail.com>
2023-09-26 10:02:02 +0300

    Completed with the set up for the speedDetector challenge

---
+++ speedDetector.js
@@ -0,0 +1,12 @@
+let carSpeed = function(speed){
+    let driverSpeed = 70;
+    let addDemerits = '';
+    if(driverSpped < 70){
+      return 'Ok';
+      addDemerits = 0;
+    }
+    else{
+      addDemerita =+ 1 && driverSpeed > 70+5;
+    }
+  }
+  
